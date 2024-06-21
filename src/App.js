f53ec5288bf806efd0fe05801624f2bfb57bd758
Not-7Myself <98397618+Not-7Myself@users.noreply.github.com>
2024-06-22 02:32:58 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import { set } from "firebase/database";
 import { useState } from "react";
 
-const items = [
-  { id: 1, desc: "passports", quantity: 2, packed: false },
-  { id: 2, desc: "octopus pillow", quantity: 1, packed: true },
-  { id: 3, desc: "charger", quantity: 2, packed: true },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function addItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function removeItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function checkItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="App">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form addItems={addItems} />
+      <PackingList
+        items={items}
+        key={items.id}
+        removeItems={removeItems}
+        checkItems={checkItems}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -22,7 +38,7 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ addItems }) {
   const [desc, setDesc] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -37,6 +53,9 @@ function Form() {
       quantity: quantity,
       packed: false,
     };
+
+    addItems(newItem);
+
     setDesc("");
     setQuantity(1);
   }
@@ -62,32 +81,73 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, removeItems, checkItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  } else if (sortBy === "desc") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a["desc"].localeCompare(b["desc"]));
+  } else {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => {
-          return <Item item={item} />;
+        {sortedItems.map((item) => {
+          return (
+            <Item
+              item={item}
+              key={item.id}
+              removeItems={removeItems}
+              checkItems={checkItems}
+            />
+          );
         })}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input Order</option>
+          <option value="desc">Sort by Description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, removeItems, checkItems }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.checked}
+        onChange={() => checkItems(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item["quantity"]} {item["desc"]}
       </span>
+      <button onClick={() => removeItems(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const itemLength = items.length;
+  const itemPacked = items.filter((item) => item.packed).length;
+
   return (
     <footer className="stats">
-      <em>Already packed X/Y items on the list</em>
+      <em>
+        Already packed {itemPacked}/{itemLength} items on the list
+      </em>
     </footer>
   );
 }
